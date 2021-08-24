8a1f3be4d81892201f0f92c267ec01e8c669d054
yuji0406 <ky19930305@gmail.com>
2021-08-24 07:20:16 +0000

    [Add]業車情報編集機能と画面の実装

--- app/controllers/shops/mypages_controller.rb
+++ app/controllers/shops/mypages_controller.rb
@@ -6,5 +6,24 @@ class Shops::MypagesController < ApplicationController
   end
 
   def update
+    current_shop.update(shop_params)
+    redirect_to action: "show"
+  end
+
+  private
+
+  def shop_params
+
+    params.require(:shop).permit(
+      :shop_name,
+      :manager,
+      :introduction,
+      :shop_image_id,
+      :shop_genre,
+      :adddress,
+      :postal_code,
+      :phone_number,
+      :email
+      )
   end
 end
