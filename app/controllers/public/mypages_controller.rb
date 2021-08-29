83048a3b28488bf675b97d6df712e61ea80f5f83
yuji0406 <ky19930305@gmail.com>
2021-08-29 05:43:19 +0000

    [Add]店舗側のマイページ、情報編集画面の実装

--- app/controllers/public/mypages_controller.rb
+++ app/controllers/public/mypages_controller.rb
@@ -6,5 +6,20 @@ class Public::MypagesController < ApplicationController
   end
 
   def update
+    current_customer.update(customer_params)
+    redirect_to mypages_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :store_name,
+      :store_manager,
+      :postal_code,
+      :address,
+      :phone_number,
+      :email
+      )
   end
 end
