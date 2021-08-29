0e37eba97130083c1335c6c66f40f69b43955f55
yuji0406 <ky19930305@gmail.com>
2021-08-29 06:26:43 +0000

    [Add]店舗側の業者一覧画面の実装

--- app/controllers/public/shops_controller.rb
+++ app/controllers/public/shops_controller.rb
@@ -1,7 +1,14 @@
 class Public::ShopsController < ApplicationController
   def index
+    @genres = Shop.distinct.pluck(:shop_genre)
+    if params[:shop_genre]
+      @shops = Shop.where(shop_genre: params[:shop_genre])
+    else
+      @shops = Shop.all
+    end
   end
 
   def show
+    @shop = Shop.find(params[:id])
   end
 end
